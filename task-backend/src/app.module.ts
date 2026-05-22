6a8a368460c31eb2faee8048ead01a6b1d28d409
Mateo <mmbecerra@sudamericano.edu.ec>
2026-05-22 17:14:34 -0500

    Actualizacion backend y prisma

--- task-backend/src/app.module.ts
+++ task-backend/src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { TasksModule } from './tasks/tasks.module';
-import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [TasksModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
 })
+
 export class AppModule {}
